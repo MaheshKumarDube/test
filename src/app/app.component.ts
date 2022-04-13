298ce89c38023dc5668707a9a603aa5f3c0cec0c
Mahesh <mahesh.dbe@gmail.com>
2022-04-13 09:57:14 +0530

    Creata a simple Todo applicaton for interview

---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+ import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ToDoPage';
+
+  public toDoForm :  FormGroup;
+  userInfo :any = [];
+  userdetail:any =[];
+  editdata: any;
+  editflag: boolean =false;
+  editInd: any;
+  updatedindex: any;
+
+  constructor(private fb:FormBuilder){
+    this.toDoForm = this.fb.group({
+      userName : new FormControl('',Validators.required),
+      userEmail : new FormControl('',[Validators.required,Validators.email]),
+      userPassword : new FormControl('',Validators.required)
+    });
+  }
+
+
+  ngOnInit(){
+    this.getlocalstoragedata();
+  }
+
+  getlocalstoragedata(){
+    this.userdetail = localStorage.getItem('userInfo');
+    this.userInfo = JSON.parse(this.userdetail);
+    console.log(this.userInfo,"userInfo");
+  }
+
+
+  formdata(toDoForm){
+   if(toDoForm && toDoForm.touched){
+    if(!this.userInfo){this.userInfo =[]; }
+      this.userInfo.push(toDoForm.value);
+      localStorage.setItem('userInfo', JSON.stringify(this.userInfo));
+      this.toDoForm.reset();
+    }
+  }
+
+  editRow(event,i){
+    this.editInd = i;
+    this.editdata = event;
+    if(this.editdata !=null){
+      this.editflag = true;
+      this.toDoForm.patchValue({
+        userName: this.editdata.userName,
+        userEmail: this.editdata.userEmail,
+        userPassword: this.editdata.userPassword,
+      });
+     
+    }
+   }
+
+  update(editdata){
+    if(editdata!=null){
+      this.userInfo[this.editInd] = editdata.value;
+     
+    }
+  }
+ }
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
